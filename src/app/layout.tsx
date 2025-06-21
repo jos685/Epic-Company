84868667b3c9bda35488dac848da9ad52e38f291
Joseph Oduor <josephowang261@gmail.com>
2025-06-21 03:23:36 +0300

    Added google anlytics script to the layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -9,6 +9,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-T9489C6B1J"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-T9489C6B1J');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <Toaster position="top-right" />
